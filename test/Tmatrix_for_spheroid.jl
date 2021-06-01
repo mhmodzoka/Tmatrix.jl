b1ea836d17791434d5ecc84f25defb8ab98d1cc3
Mahmoud Elzouka <mahmoud.elzouka@huskers.unl.edu>
2021-06-01 10:02:32 -0700

    trying to make it work with BigFlaot

--- test/Tmatrix_for_spheroid.jl
+++ test/Tmatrix_for_spheroid.jl
@@ -6,3 +6,9 @@ k1 = 1e7
 k2 = 1.5e7 + 1e3 * im
 
 @time T = calculate_Tmatrix_for_spheroid(rx, rz, n_max, k1, k2)
+
+rx, rz = BigFloat(1e-6), BigFloat(1.3e-6)
+n_max = 3
+k1 = BigFloat(1e7) + 0*im
+k2 = BigFloat(1.5e7) + BigFloat(1e3) * im
+@time T = calculate_Tmatrix_for_spheroid(rx, rz, n_max, k1, k2)
