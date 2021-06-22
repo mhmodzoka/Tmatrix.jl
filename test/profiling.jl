a4b8eb223346460c86eca1eaff7a1c09971e1248
Mahmoud Elzouka <mahmoud.elzouka@huskers.unl.edu>
2021-06-22 16:28:05 -0700

    benchmarking

--- test/profiling.jl
+++ test/profiling.jl
@@ -1,20 +1,21 @@
 # %%
 using Tmatrix
-using Profile
+using ProfileView
+
 
 rx, rz = 1e-7, 1.3e-7
-n_max = 3
+n_max = 8
 k1 = Complex(6.2e6)
 k2 = 7e6 + 1e3 * im
 rotationally_symmetric = true
 
-calculate_Tmatrix_for_spheroid(rx, rz, n_max, k1, k2; rotationally_symmetric=rotationally_symmetric)
+@profiler calculate_Tmatrix_for_spheroid(rx, rz, n_max, k1, k2; rotationally_symmetric=rotationally_symmetric)
 
 
 """
 using VectorSphericalWaves
 n_θ_points, n_ϕ_points = 10000,20000
-θ_array, ϕ_array = Tmatrix.meshgrid_θ_ϕ(n_θ_points, n_ϕ_points; min_θ=1e-16, min_ϕ=1e-16, rotationally_symmetric=rotationally_symmetric) 
+θ_array, ϕ_array = Tmatrix.meshgrid_θ_ϕ(n_θ_points, n_ϕ_points; min_θ=1e-16, min_ϕ=1e-16, rotationally_symmetric=rotationally_symmetric)
 r_array, n̂_array = Tmatrix.ellipsoid(rx, rz, θ_array);
 VectorSphericalWaves.N_mn_wave_SVector.(0,1,k2*r_array, θ_array,ϕ_array; kind= "regular")
 VectorSphericalWaves.N_mn_wave_SVector(0,1,k2*r_array[1], θ_array[1],ϕ_array[1]; kind= "regular")
