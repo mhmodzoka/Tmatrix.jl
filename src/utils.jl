a405c8623a46833cae34fca83573eb5f9f06c8e5
Collin A Guo <bunjuku8@gmail.com>
2021-09-28 13:09:59 -0700

    added vecvec_to_matrix

--- src/utils.jl
+++ src/utils.jl
@@ -2,6 +2,21 @@
 
 using LinearAlgebra
 
+"""
+Converts vector of vectors (esp. internal vectors [x, y, z]) to matrix of correct form for convert
+"""
+
+function vecvec_to_matrix(vecvec)
+    dim1 = length(vecvec)
+    dim2 = length(vecvec[1])
+    my_array = zeros(Int64, dim1, dim2)
+    for i in 1:dim1
+        for j in 1:dim2
+            my_array[i,j] = vecvec[i][j]
+        end
+    end
+    return my_array
+end
 
 """
 Converts to spherical
